4be17e4ee8b0843b341291d16fb653955930c944
Lami Qv <lamiqv@MBP-Lami.Dlink>
2020-01-30 23:57:20 +0300

    ducatus design

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();   
+$(document).ready(() => {
+  $('[data-toggle="tooltip"]').tooltip();
+
+//   $('.footer-menu').click((e) => {
+
+//     if (e.toElement.classList.contains('footer-menu')) {
+
+//       if (e.toElement.parentElement.classList.contains('footer-menu-same') && !e.toElement.parentElement.classList.contains('footer-menu-same-open')) {
+//         e.toElement.parentElement.classList.add('footer-menu-same-open');
+//       } else if (e.toElement.parentElement.classList.contains('footer-menu-same') && e.toElement.parentElement.classList.contains('footer-menu-same-open')) {
+//         e.toElement.parentElement.classList.remove('footer-menu-same-open');
+//       }
+
+//       if (e.toElement.classList.contains('footer-menu-open')) {
+//         e.toElement.classList.remove('footer-menu-open');
+//       } else {
+//         e.toElement.classList.add('footer-menu-open');
+//       }
+
+//     }
+
+//   });
 });
